70f2e6bc71e1bf1f6b9354ddd9c289cbae4469ed
riyabaheti <51139731+riyabaheti@users.noreply.github.com>
2020-09-14 12:37:05 -0700

    updated

--- Malaria_Classification.py
+++ Malaria_Classification.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 import joblib
 
